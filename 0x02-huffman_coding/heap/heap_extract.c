aacba37efb2c3d1c1e3a5991ef2a31e66c8902e1
JwowSquared <Jdwagner1@hotmail.com>
2021-12-31 11:46:09 +0000

    now correctly extracts by moving the last node to the top and re-heapifying the tree

--- 0x02-huffman_coding/heap/heap_extract.c
+++ 0x02-huffman_coding/heap/heap_extract.c
@@ -9,38 +9,41 @@
 void *heap_extract(heap_t *heap)
 {
 	void *data, *temp;
-	binary_tree_node_t *out, *swap;
+	binary_tree_node_t *out, *swap, *last;
 
 	if (heap == NULL || heap->root == NULL)
 		return (NULL);
 
 	data = heap->root->data;
 	out = heap->root;
+	last = find_parent(heap, heap->size, heap->size);
+	if (last == NULL)
+		last = heap->root;
+	else if (heap->size % 2 == 0)
+	{
+		last = last->left;
+		last->parent->left = NULL;
+	}
+	else
+	{
+		last = last->right;
+		last->parent->right = NULL;
+	}
+	out->data = last->data;
+
 	while (out->left != NULL)
 	{
 		swap = out->left;
-		if (out->right != NULL && heap->data_cmp(swap->data, out->right->data) >= 0)
+		if (out->right != NULL && heap->data_cmp(swap->data, out->right->data) > 0)
 			swap = out->right;
+		if (heap->data_cmp(out->data, swap->data) < 0)
+			break;
 		temp = out->data;
 		out->data = swap->data;
 		swap->data = temp;
 		out = swap;
 	}
-	swap = find_parent(heap, heap->size, heap->size);
-	if (swap == NULL)
-		swap = heap->root;
-	else if (heap->size % 2 == 0)
-	{
-		swap = swap->left;
-		swap->parent->left = NULL;
-	}
-	else
-	{
-		swap = swap->right;
-		swap->parent->right = NULL;
-	}
-	out->data = swap->data;
-	free(swap);
+	free(last);
 	heap->size = heap->size - 1;
 	if (heap->size == 0)
 		heap->root = NULL;
